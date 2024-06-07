a0b8e94eb891c4a15d0a19a03eb0a0712e7117bf
Atia1978 <abdalla_atia@hotmail.com>
2024-06-07 10:28:48 +0200

    #2 I declare Emun in  new file

---
+++ Project4/SelectMode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Project4
+{
+   
+        public enum SelectMode
+        {
+
+            MiddleHorizontal = 1,
+            AllHorizontal = 2,
+            AllVerticals = 3,
+            Diagonals = 4,
+            Jackpot = 5
+        }
+    }
+
